d93d347b1c44b9c1da5cc16f0b2dbd3283be772c
David Sotelo <juandavid.sotelo.m@gmail.com>
2022-05-17 09:34:33 -0500

    Second commit2

---
+++ T1/s.js
@@ -0,0 +1,33 @@
+// const RE_IPv4 = new RegExp(
+//   "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+// );
+
+const miFun = () => {
+  if (validateIp(document.getElementById("txtInputIp").value)) {
+    document.getElementById("txtInputIp").classList.add("correct");
+    document.getElementById("txtInputIp").classList.remove("incorrect");
+  } else {
+    document.getElementById("txtInputIp").classList.remove("correct");
+    document.getElementById("txtInputIp").classList.add("incorrect");
+  }
+  validateIp(document.getElementById("txtInputIp").value);
+};
+
+const validateIp = (parTxt) => {
+  const d = parTxt.split(".").map((e) => parseInt(e));
+  return (
+    parTxt.length < 15 &&
+    !/\s/.test(parTxt) &&
+    d.length === 4 &&
+    validateIpArrange(d)
+  );
+};
+
+const validateIpArrange = (parArrange) => {
+  const e = parArrange.pop();
+  if (parArrange.length === 0) {
+    return e >= 0 && e <= 255;
+  } else {
+    return e >= 0 && e <= 255 && validateIpArrange(parArrange);
+  }
+};
